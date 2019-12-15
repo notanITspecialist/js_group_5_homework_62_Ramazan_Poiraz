c2665fa3ff06002e443395776a65cf2101ba41c6
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-15 10:29:18 +0300

    Добавил роуты

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+
+
 import ListOfCountries from "./components/listOfCountries/listOfCountries";
 import CreateBurger from "./components/burger/createBurger";
 
 function App() {
   return (
-    <div>
-      <ListOfCountries />
-      <CreateBurger />
-    </div>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact render={()=><div>HOME PAGE</div>} />
+            <Route path="/ListOfCountries" component={ListOfCountries} />
+            <Route path="/CreateBurger" component={CreateBurger} />
+        </Switch>
+    </BrowserRouter>
   );
 }
 
